75ae874763a278dd8166c84dc711f3ccfca3efe7
doba <doba.fullstack@gmail.com>
2021-09-29 13:39:30 +0700

    Fix Conflix

---
+++ furniture_v2_be/src/Routes/index.routes.ts
@@ -0,0 +1,15 @@
+import ProductRoute from "./Product.routes";
+import AuthRoute from "./Auth.routes";
+import CateRoute from "./Category.routes";
+import SocialMediaRoute from "./SocialMedia.routes";
+import BillRoute from "./Bill.routes";
+import { Express } from "express";
+import PATH from "../Constant/url";
+
+export default (app: Express) => {
+    app.use(PATH.PRODUCT.baseURL, ProductRoute);
+    app.use(PATH.CATEGORY.baseURL, CateRoute);
+    app.use(PATH.AUTH.baseURL, AuthRoute);
+    app.use(PATH.SOCIAL_MEDIA.baseURL, SocialMediaRoute);
+    app.use(PATH.BILL.baseURL, BillRoute);
+};
