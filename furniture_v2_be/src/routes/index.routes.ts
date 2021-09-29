2593fe6ef0388ecbe31be0e8b14aaf4d5895a858
doba <doba.fullstack@gmail.com>
2021-09-29 13:24:01 +0700

    Doba

--- furniture_v2_be/src/routes/index.routes.ts
+++ furniture_v2_be/src/routes/index.routes.ts
@@ -1,13 +1,11 @@
-import AuthRoute from "./Auth.routes";
-import ProductRoute from "./Product.routes";
-import CategoryRoute from "./Category.routes";
-import BillRoute from './Bill.routes'
-import { Express } from "express";
-import PATH from "../Constant/url";
+import ProductRoute from './Product.routes';
+import AuthRoute from './Auth.routes';
+import CategoryRoute from './Category.routes'
+import {Express} from 'express';
+import PATH from '../Constant/url'
 
 export default (app: Express) => {
-    app.use(PATH.AUTH.baseURL, AuthRoute);
     app.use(PATH.PRODUCT.baseURL, ProductRoute);
+    app.use(PATH.AUTH.baseURL, AuthRoute);
     app.use(PATH.CATEGORY.baseURL, CategoryRoute);
-    app.use(PATH.BILL.baseURL, BillRoute);
 };
