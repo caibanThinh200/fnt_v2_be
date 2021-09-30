9f23e815f8e963cda2580af4180eb199f206e364
Nguyễn Quốc Thịnh <thinh.nguyen@joyvn.vn>
2021-09-30 14:07:30 +0700

    fix bug

--- furniture_v2_be/src/app.ts
+++ furniture_v2_be/src/app.ts
@@ -5,7 +5,7 @@ import CommonUtils from "./Utils/function";
 import logger from './config/logger';
 import colors, { red } from "colors";
 import TAG_DEFINE from './Constant/define';
-import dataConfig from "./config/mongodb";
+import dataConfig from "./Config/mongodb";
 import PATH from './Constant/url';
 import bodyParser from "body-parser";
 import router from './Routes/index.routes'
