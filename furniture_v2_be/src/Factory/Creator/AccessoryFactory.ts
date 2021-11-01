19cc14685e1caf9d4c17a89b35a49b6363b83cad
doba <doba.fullstack@gmail.com>
2021-11-01 12:16:20 +0700

    Add AA-pet side

--- furniture_v2_be/src/Factory/Creator/AccessoryFactory.ts
+++ furniture_v2_be/src/Factory/Creator/AccessoryFactory.ts
@@ -2,10 +2,16 @@ import TAG_DEFINE from '../../Constant/define';
 import AccessoryRequest from '../Concreate/Furniture/Request/accessory';
 import AccessoryResponse from '../Concreate/Furniture/Response/accessory';
 import FurnitureAccessoryModel, { FurnitureAccessorySchema } from '../../models/Accessory/furniture';
+
+//AA-PET
+import AAPetRequest from '../Concreate/AA-PET/Request/accessory'
+import AAPetResponse from '../Concreate/AA-PET/Response/accessory'
+import AAPetModel from '../../models/Accessory/aa-pet';
 export class AccessoryFactory {
     public static createAccessory(data, type) {
         switch(type) {
             case TAG_DEFINE.STORE.FURNITURE: return new AccessoryRequest(data);
+            case TAG_DEFINE.STORE.AA_PET: return new AAPetRequest(data);
             default: return new AccessoryRequest(data);
         }
     }
@@ -13,6 +19,7 @@ export class AccessoryFactory {
     public static getAccessory(data, type) {
         switch(type) {
             case TAG_DEFINE.STORE.FURNITURE: return new AccessoryResponse(data);
+            case TAG_DEFINE.STORE.AA_PET: return new AAPetResponse(data);
             default: return new AccessoryResponse(data);
         }
     }
@@ -20,6 +27,7 @@ export class AccessoryFactory {
     public static createSchema(data, type) {
         switch(type) {
             case TAG_DEFINE.STORE.FURNITURE: return new FurnitureAccessoryModel(data);
+            case TAG_DEFINE.STORE.AA_PET: return new AAPetModel(data);
             default: return new FurnitureAccessoryModel(data);
         }
     }
@@ -27,6 +35,7 @@ export class AccessoryFactory {
     public static getSchema(type) {
         switch(type) {
             case TAG_DEFINE.STORE.FURNITURE: return FurnitureAccessoryModel;
+            case TAG_DEFINE.STORE.AA_PET: return AAPetModel;
             default: return FurnitureAccessoryModel;
         }
     }
