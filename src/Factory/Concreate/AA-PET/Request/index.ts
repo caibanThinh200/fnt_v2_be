47ca69a42e7277835bf45d0b27ea53256b5d4f77
doba <doba.fullstack@gmail.com>
2021-12-25 14:19:28 +0700

    Fix root

--- src/Factory/Concreate/AA-PET/Request/index.ts
+++
@@ -1,5 +0,0 @@
-import Product from './product'
-import User from './user'
-
-export const AAPetProductRequest = Product;
-export const AAPetUserRequest = User;
--- src/Factory/Concreate/AA-PET/Response/index.ts
+++
@@ -1,5 +0,0 @@
-import Product from "./product";
-import User from "./user";
-
-export const AAPetProductResponse = Product;
-export const AAPetUserResponse = User;
